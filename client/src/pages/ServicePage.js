162141ab22f0df49a47e9e05654736da04211b71
Joyal <georgejoyal31@gmail.com>
2024-08-02 18:14:44 +0530

    Done: Service page and Navbar

--- client/src/pages/ServicePage.js
+++ client/src/pages/ServicePage.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import ServiceCardContainer from "../components/ServiceCardContainer";
 
 function ServicePage() {
+  const Cards = {
+    items: [
+      {
+        title: "title",
+        location: "location",
+        workingTime: "09:00 AM - 09:00 PM",
+        workingDay: "Monday - Friday",
+      },
+      {
+        title: "title",
+        location: "location",
+        workingTime: "09:00 AM - 09:00 PM",
+        workingDay: "Monday - Friday",
+      },
+      {
+        title: "title",
+        location: "location",
+        workingTime: "09:00 AM - 09:00 PM",
+        workingDay: "Monday - Friday",
+      },
+    ],
+    toys: [
+      {
+        title: "title",
+        location: "location",
+        workingTime: "09:00 AM - 09:00 PM",
+        workingDay: "Monday - Friday",
+      },
+      {
+        title: "title",
+        location: "location",
+        workingTime: "09:00 AM - 09:00 PM",
+        workingDay: "Monday - Friday",
+      },
+      {
+        title: "title",
+        location: "location",
+        workingTime: "09:00 AM - 09:00 PM",
+        workingDay: "Monday - Friday",
+      },
+      {
+        title: "title",
+        location: "location",
+        workingTime: "09:00 AM - 09:00 PM",
+        workingDay: "Monday - Friday",
+      },
+      {
+        title: "title",
+        location: "location",
+        workingTime: "09:00 AM - 09:00 PM",
+        workingDay: "Monday - Friday",
+      },
+    ],
+  };
+
+  const responseKeys = Object.keys(Cards);
+
   return (
-    <div>ServicePage</div>
-  )
+    <div className="flex flex-row bg-neutral-100">
+      <Navbar />
+      <div className="pt-8 w-5/6 overflow-hidden">
+        <p className="text-3xl text-neutral-700 font-medium">Services</p>
+          {responseKeys.map((card, ind) => <ServiceCardContainer categoryTitle={card} category={Cards[card]} key={ind} />)}
+      </div>
+    </div>
+  );
 }
 
-export default ServicePage
+export default ServicePage;
